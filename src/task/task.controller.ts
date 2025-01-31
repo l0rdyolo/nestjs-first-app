f47c7f6197af437fcc138efec24408846273be5d
Omer Faruk Cinoglu <62906173+l0rdyolo@users.noreply.github.com>
2025-01-31 19:39:34 +0300

    Update task.controller.ts

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { TaskService } from './task.service';
+import { Task } from './task.model';
 
 @Controller('task')
-export class TaskController {}
+export class TaskController {
+    constructor(private readonly tasksService : TaskService){}
+
+    @Get()
+    getAllTasks() : Task[]{
+        return this.tasksService.getAllTasks();
+    }
+
+    @Get(':id')
+    getTaskById(@Param('id') id: string): Task | undefined {
+      return this.tasksService.getTaskById(id);
+    }
+}
